df036772cc31963b446932a62edbe077f91b2565
Kamil Pludra <pludra@gmail.com>
2012-12-28 14:17:58 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YourBunch::Application.config.secret_token = '2f649b22a9250ee96d987ddc2fbf26a7e7756b1dd450df4b34405cb91e0847acb43bfd08e6f8d26fddfdcf75b3ba624bb775e26303f3c3ffd6dda85f83d1f69e'
